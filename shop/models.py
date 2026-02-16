d07e34556197f135ee35751df52efaaf2e99c062
Zee <amirabaszarkhah268@gmail.com>
2026-02-16 11:54:42 +0330

    Add all changes including template, models.py and views.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="products/")
+    slug = models.SlugField(unique=True,allow_unicode=True)
+
+    def __str__(self):
+        return self.title
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def index_view(request):
+#     context = {
+#         'popular_games':Product.objects.all()
+#     }
+#     return render(request,"index.html")
